202667fb561efc54761c7c2916a0ede5ac6a46dd
Guilherme Souza <gsouza2651@gmail.com>
2025-02-27 17:22:34 -0300

    add: About page

---
+++ src/pages/app/about/index.tsx
@@ -0,0 +1,18 @@
+export function About() {
+  return (
+    <div className="flex flex-col gap-6 items-center justify-center mx-auto max-w-6xl h-full">
+      <h1 className="text-center font-bold text-4xl">About</h1>
+      <p className="text-xl text-center">
+        Welcome to REST Countries API Project, your ultimate destination for
+        discovering detailed information about countries around the world! 🌎✨
+      </p>
+      <p className="text-xl text-center">
+        Powered by the REST Countries API, this project provides up-to-date
+        details on each nation, including official names, populations,
+        currencies, languages, and more. Whether you're a traveler, researcher,
+        or just curious about global diversity, Country Explorer makes it easy
+        to explore and learn.
+      </p>
+    </div>
+  )
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,5 @@
 import { NotFound } from '@/pages/404'
+import { About } from '@/pages/app/about'
 import { Home } from '@/pages/app/home'
 import { ErrorPage } from '@/pages/error-page'
 import { AppLayout } from '@/pages/layouts/app-layout'
@@ -16,7 +17,7 @@ export const routes = createBrowserRouter([
         path: '/countries/:countryCode',
         element: <h1>country detail</h1>,
       },
-      { path: '/about', element: <h1>about</h1> },
+      { path: '/about', element: <About /> },
     ],
   },
   {
